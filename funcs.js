a21140663c8663db0787e916b7911cca80edcbda
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-14 14:35:28 +0100

    feat: refactored code to pass tests

--- funcs.js
+++ funcs.js
@@ -1,13 +1,33 @@
 function isFive(num) {
   // Your code here
+if(num===5){
+return true;
+}
+return false;
 }
 
 function isOdd(number) {
   // Your code here
+  
+  if(typeof number !== "number"){
+     throw new Error("Wrong input, Please enter a number!");
+  }
+
+	  else if(number%2===0){
+   return false;
+   }
+  return true;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+      let answer=[];
+	if(min<max){
+        for(let i=min ; i<=max; i+=step){
+         answer.push(i);
+	}
+	}
+  return answer;
 }
 
 
